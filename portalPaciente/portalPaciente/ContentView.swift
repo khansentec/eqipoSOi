0477ba1874a11f08b33692e5d6d3d872a70fcb71
katie <a01760829@tec.mx>
2022-09-06 13:44:42 -0500

    Navigación sencilla

--- portalPaciente/portalPaciente/ContentView.swift
+++ portalPaciente/portalPaciente/ContentView.swift
@@ -7,14 +7,38 @@
 
 import SwiftUI
 
+struct ResultView: View {
+    var choice : String
+    
+    var body: some View {
+        Text("You chose \(choice)")
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Bienvenido")
-            .fontWeight(.bold)
-            .font(.largeTitle)
-            .padding()
-        Text("al portal de pacientes")
-            .padding()
+        //        Text("Bienvenido")
+        //            .fontWeight(.bold)
+        //            .font(.largeTitle)
+        //            .padding()
+        //        Text("al portal de pacientes")
+        //            .padding()
+        //        Text("Por favor, elija una pagina para abrir.")
+        //
+        NavigationView {
+            VStack(spacing: 30) {
+                Text("Por favor, seleccione una página para abrir")
+                NavigationLink(destination: Text("Entre una medición para agregar: ")) {
+                    Text("Vista 1: Agregar medición")
+                }
+                NavigationLink(destination: Text("Elija un doctor: ")) {
+                    Text("Vista 2: Platicar con doctor vinculado")
+                }
+            }
+            .navigationBarTitle("Navigación")
+            
+        }
+        
     }
 }
 
