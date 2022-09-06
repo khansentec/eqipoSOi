231489d6a0b7c7fe77782ab71bed27c0252011f1
katie <a01760829@tec.mx>
2022-09-06 10:58:19 -0500

    Commit inicial

---
+++ portalPaciente/portalPaciente/portalPacienteApp.swift
@@ -0,0 +1,17 @@
+//
+//  portalPacienteApp.swift
+//  portalPaciente
+//
+//  Created by Katie Hansen on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct portalPacienteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
