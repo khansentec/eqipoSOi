4788e07dc28bdbe4ea09a02a4c84f47d00235e84
katie <a01760829@tec.mx>
2022-09-06 11:47:32 -0500

    texto de bienvenido

--- portalPaciente/portalPaciente/ContentView.swift
+++ portalPaciente/portalPaciente/ContentView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Bienvenido")
+            .fontWeight(.bold)
+            .font(.largeTitle)
+            .padding()
+        Text("al portal de pacientes")
             .padding()
     }
 }
