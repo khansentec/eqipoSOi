916225e0962aee6cbe6b281394968927568bfdb9
khansentec <112965878+khansentec@users.noreply.github.com>
2022-09-21 11:02:15 -0500

    Acabó vistas

--- portalPaciente/portalPaciente/Katie vistas/VistaPreguntasFrecuentes.swift
+++ portalPaciente/portalPaciente/Katie vistas/VistaPreguntasFrecuentes.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct VistaPreguntasFrecuentes: View {
+    
+    @State var listaPreguntas : [[String]]
+    
     var body: some View {
         NavigationView {
-            
-            VStack (alignment: .leading, spacing: 7) {
-                Text("1. ¿Cómo puedo cerrer una sesión?")
-                    .fontWeight(.bold)
-                Text("Da clic en el butón 'Cerrar sesión'.")
+            VStack (alignment: .leading) {
+                
+                ForEach(listaPreguntas, id: \.self) {
+                    pregunta in
+                    VStack (alignment: .leading, spacing: 10){
+                        Text(pregunta[0])
+                            .fontWeight(.bold)
+                        Text(pregunta[1])
+                    }
+                    .padding()
+                    Divider()
+                }
+                
+                Spacer()
             }
+            .scenePadding()
             .navigationTitle("Preguntas frecuentes")
         }
     }
@@ -23,7 +36,13 @@ struct VistaPreguntasFrecuentes: View {
 
 struct VistaPreguntasFrecuentes_Previews: PreviewProvider {
     static var previews: some View {
-        VistaPreguntasFrecuentes()
+        
+        let ejemplosPreguntas = [
+            ["P: ¿Cómo puedo cerrer una sesión?", "R: Da clic en el butón 'Cerrar sesión'."],
+            ["P: No tengo un médico vinculado. ¿Cómo puedo conseguir uno?", "R: Naviga a la página de Médico vinculado y da clic en el búton 'Registrar'."]
+        ]
+        
+        VistaPreguntasFrecuentes(listaPreguntas: ejemplosPreguntas)
     }
 }
 
