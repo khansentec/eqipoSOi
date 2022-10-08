3549c725fe5176d0adffb8463756f2716dcab824
AndreaCorona523 <85129680+AndreaCorona523@users.noreply.github.com>
2022-10-08 16:45:44 -0500

    med add init

--- Agenda Arterial V2.0/Data Structures/Medicament.swift
+++ Agenda Arterial V2.0/Data Structures/Medicament.swift
@@ -1,24 +1,37 @@
 //
 //  Medicament.swift
-//  Agenda Arterial V1
+//  Agenda Arterial V2.0
 //
 //  Created by Gabriel Crisostomo on 19/09/22.
 //
 
 import Foundation
 
-struct Medicament : Hashable, Identifiable {
-    var id = UUID()
-    var idPacient : String
+struct Medicament : Identifiable, Hashable {
+    var id : String
     var medicamentName : String
     var information : String
     var startDate : Date
+    var finishDate : Date
     var forgetTimes : Int
+
+    init(id: String?, medicamentName: String, information: String, startDate: Date, finishDate: Date, forgetTimes: Int){
+        self.id = id ?? ""
+        self.medicamentName = medicamentName
+        self.information = information
+        self.startDate = startDate
+        self.finishDate = finishDate
+        self.forgetTimes = forgetTimes
     }
+    
+}
+
+
+
     
 extension Medicament {
     static let medicamentosList = [
-        Medicament(idPacient: "1234", medicamentName: "Paracetamol", information: "sjhf", startDate: Date.now, forgetTimes: 12)
+        Medicament(id: "12", medicamentName: "Paracetamol", information: "sjhf", startDate: Date.now, finishDate: Date.now,forgetTimes: 12)
         
     ]
 }
